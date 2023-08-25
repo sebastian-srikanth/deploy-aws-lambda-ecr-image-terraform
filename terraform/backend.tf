41338ca9403e246e2d77aca5dffa12976d21d6b6
Sebastian Srikanth Kumar Selvaraj <sebastiansrikanth@Sebastians-Mac-mini.local>
2023-08-25 13:07:53 +0530

    initialize github action

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,14 @@
 terraform {
     required_version = ">= 1.5.6"
-    backend "s3" {
-        bucket = "${var.backend_bucket_name}"
-        key = "${var.backend_bucket_key}"
-        region = "${var.aws_region}"
+
+    locals {
+        s3_bucket_name = var.backend_bucket_name
+        s3_key = var.backend_bucket_key
+        s3_region = var.aws_region
     }
+    backend "s3" {
+        bucket = local.s3_bucket_name
+        key = local.s3_key
+        region = local.s3_region
+  }
 }
